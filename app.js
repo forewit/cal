1f6d2e480002d2de4102eb53c3cef09f5a103979
Anderson <MDAnderson@samaritan.org>
2021-08-30 14:56:36 -0400

    na

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 // Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-app.js";
 import { getAuth, signInWithPopup, GithubAuthProvider, signOut } from 'https://www.gstatic.com/firebasejs/9.0.0/firebase-auth.js'
+import { getFirestore, setDoc, getDoc, doc } from 'https://www.gstatic.com/firebasejs/9.0.0/firebase-firestore.js'
 
-// Setup Firebase Auth
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAsBkU7NKqKn9gAV07V_21AkQMw3ZJQ9hQ",
+    authDomain: "canvas-2fdd2.firebaseapp.com",
+    databaseURL: "https://canvas-2fdd2.firebaseio.com",
+    projectId: "canvas-2fdd2",
+    storageBucket: "canvas-2fdd2.appspot.com",
+    messagingSenderId: "989212373436",
+    appId: "1:989212373436:web:2491b895c5a13ec3007bb9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 const provider = new GithubAuthProvider();
 const auth = getAuth();
+const db = getFirestore(app);
+
 
 // Track auth state -------------------------------
 window.onload = function () {
@@ -12,10 +28,16 @@ window.onload = function () {
             // User is signed in.
             document.getElementById('account-details').textContent = 'Signed in as: ' + user.displayName;
             document.getElementById('sign-in-button').textContent = 'Sign out';
+
+            document.getElementById('login-count').classList.remove('hidden');
+            getLoginCount(user.uid).then(function (data) {
+                document.getElementById('login-count').textContent = 'Login count: ' + data;
+            });
         } else {
             // User is signed out.
             document.getElementById('account-details').textContent = 'Signed out';
             document.getElementById('sign-in-button').textContent = 'Sign in with GitHub';
+            document.getElementById('login-count').classList.add('hidden');
         }
     }, function (error) {
         console.log(error);
@@ -24,7 +46,7 @@ window.onload = function () {
 // ------------------------------------------------
 
 
-// when sign in button is clicked, then sign in with GitHub
+// Sign in and out -------------------------------
 document.getElementById('sign-in-button').addEventListener('click', function () {
     if (auth.currentUser) {
         // User is signed in.
@@ -68,6 +90,38 @@ document.getElementById('sign-in-button').addEventListener('click', function () 
         // -------------------------------------------------
     }
 });
+// ------------------------------------------------
+
+
+// read data from firestore ------------------------
+async function getLoginCount(uid) {
+    const docRef = doc(db, "users", uid);
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+        let oldCount = docSnap.data().loginCount;
+        let newCount = oldCount + 1;
+
+        // update the document
+        await setDoc(doc(db, "users", uid), { loginCount: newCount });
+
+        console.log("Incrementing user login count to: ", newCount);
+        return newCount;
+    } else {
+        // doc.data() will be undefined in this case
+
+        // create a new document
+        await setDoc(doc(db, "users", uid), { loginCount: 1 });
+
+        console.log("Document doesn't exist, creating: ", uid);
+        return 1;
+    }
+}
+// ------------------------------------------------
+
+async function incrementLoginCount(uid) {
+
+}
 
 /*
 // Get a reference to the database service
